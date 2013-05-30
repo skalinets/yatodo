49de0bc9dd36099e5ee475cc57b1d42b95cd1627
Serhiy Kalinets <skalinets@gmail.com>
2013-05-31 00:26:07 +0300

    adds psake build for acceptance tests, sample site, webdriver tests

--- build/default.ps1
+++ build/default.ps1
@@ -1,10 +1,21 @@
 properties {
-  $src = "..\src"
-  $solution = $src + "\yatodo.sln"
+  $srcDir = "..\src"
+  $solution = $srcDir + "\yatodo.sln"
+  $buildconfiguration = "Debug"
 }
 
-task default -depends build 
+task default -depends run-acceptance-tests 
 
 task build {
-  msbuild $solution /v:m /m
+  exec { & msbuild $solution /v:m /m  /p:configuration=$buildConfiguration }
+}
+
+task run-acceptance-tests -depends build {
+    $testDlls = ls "$srcDir\*\bin\$buildConfiguration" -rec `
+    | where { $_.Name.EndsWith(".Tests.dll") } `
+    | where { (Test-Path ([System.IO.Path]::GetDirectoryName($_.FullName) + "\Machine.Specifications.dll")) -eq $True } `
+    | foreach { $_.FullName }
+
+    $mspecExePath = Join-Path $srcDir "packages\Machine.Specifications.0.5.12\tools\mspec-clr4.exe"
+    exec { & $mspecExePath $testDlls }
 }
---
+++ src/Yatodo.Tests/WebEngine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using FluentAutomation;
+using Machine.Specifications;
+
+namespace Yatodo.Tests
+{
+    public class WebEngine : FluentTest
+    {
+        static WebEngine()
+        {
+            SeleniumWebDriver.Bootstrap();
+            SeleniumWebDriver.SelectedBrowser = SeleniumWebDriver.Browser.Chrome;
+            Settings.MinimizeAllWindowsOnTestStart = false;
+        }
+
+        public void test_fluent_automation()
+        {
+            I.Open("http://google.com");
+        }
+    }
+
+    [Subject("Integration")]
+    public class when_user_opens_page
+    {
+        static WebSite site = new WebSite();
+        
+        Because of = () => site.Open();
+
+        It should_see_empty_task_list = () => site.Tasks.Should().BeEmpty();
+    }
+
+    internal class WebSite
+    {
+        private WebEngine webEngine;
+
+        public IEnumerable<TodoTask> Tasks
+        {
+            get { return new List<TodoTask>(); }
+        }
+
+        public WebSite()
+        {
+            this.webEngine = new WebEngine();
+
+        }
+
+        public void Open()
+        {
+            
+        }
+    }
+
+    internal class TodoTask
+    {
+    }
+}
